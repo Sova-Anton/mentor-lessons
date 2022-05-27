a01051bf4b18ffa878d1669dedc16151b4c5e122
EkaterinaZi <kat.zinovik@gmail.com>
2022-05-27 20:54:37 +0300

    task-2

--- js/script.js
+++ js/script.js
@@ -17,7 +17,7 @@
 //строку в формате часов и минут
 // https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/String/padStart
 // 70 === 01:10
-const input = 70;
+/*const input = 70;
 const hours = Math.floor(input / 60);
 const minutes = input % 60;
 const modifyHours = String(hours).padStart(2, 0);
@@ -25,4 +25,19 @@ const modifyMinutes = String(minutes).padStart(2, 0);
 
 
 // console.log(modifyMinutes);
-console.log(`${modifyHours}:${modifyMinutes}`);
+console.log(`${modifyHours}:${modifyMinutes}`);*/
+
+//Напишите цикл, который выводит в консоль
+//числа от max до min по убыванию
+// Додайте все четные числа от min до max
+const max = 50;
+const min = 25;
+let total = 0;
+for (let i = max; i >= min; i -= 1){
+
+    if (i % 2 !== 0) {
+       continue
+    } 
+    total += i;
+}
+console.log(total);
