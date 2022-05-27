04ff914dae6d6cce44ae08e6ada0cf0516fa7c8d
Tripoli80 <Tripoli80@gmail.com>
2022-05-27 20:18:19 +0200

    test 4

--- js/script.js
+++ js/script.js
@@ -30,14 +30,71 @@ console.log(`${modifyHours}:${modifyMinutes}`);*/
 //Напишите цикл, который выводит в консоль
 //числа от max до min по убыванию
 // Додайте все четные числа от min до max
-const max = 50;
-const min = 25;
-let total = 0;
-for (let i = max; i >= min; i -= 1){
+// const max = 50;
+// const min = 25;
+// let total = 0;
+// for (let i = max; i >= min; i -= 1){
 
-    if (i % 2 !== 0) {
-       continue
-    } 
-    total += i;
+//     if (i % 2 !== 0) {
+//        continue
+//     }
+//     total += i;
+// }
+// console.log(total);
+
+// Напишите код, который будет спрашивать
+//логин с помощью prompt и логировать результат
+//в консоль браузера
+
+const inputLogin = prompt("Дай логін");
+console.log(`Твій логін ${inputLogin}`);
+
+switch (inputLogin) {
+    case "Admin":
+        const inputPass = prompt("Дай Пароль");
+        switch (inputPass) {
+            case "Я главный":
+                console.log("Здравствуйте!");
+                break;
+            default:
+                console.log("Неверный пароль!");
+        }
+        break;
+    case null: 
+        console.log("Отменено");
+        break;
+    
+    default:
+        console.log("Я вас не знаю");   
+        
 }
-console.log(total);
+
+// if (inputLogin === "Admin") {
+//     const inputPass = prompt("Дай Пароль");
+//     if (inputPass === "Я главный") {
+//         console.log("Здравствуйте!");
+//     } else {
+//         console.log("Неверный пароль!");
+//     }
+    
+    
+// } else if (inputLogin === null) {
+//   console.log("Отменено");
+// } else {
+//   console.log("Я вас не знаю");
+// }
+
+
+
+//Если посетитель вводит "Админ",
+//то prompt запрашивает пароль.
+//Если ничего не ввели или нажата клавиша Esc
+//вывести стороку "Отменено"
+//В противном случае вывксти строку "Я вас не знаю"
+
+//Пароль проверять так:
+//Если введён пароль "Я главный",
+//то вывести строку "Здравствуйте!"
+//иначе выводить строку "Неверный пароль!"
+
+// const loginInput = prompt("Введите свой логин");
