f5f1a899895a9006c819491c4825e4729e3c2b4c
Sova-Anton <anton.sova.goit@gmail.com>
2022-05-27 20:18:48 +0300

    task1

--- js/script.js
+++ js/script.js
@@ -1,3 +1,13 @@
-console.log(`Kdsfhdhd`)
+//Используя функцию if...else,
+//напишите код, который будет спрашивать:
+//"Какое официальное название JavaScript?"
+//Если пользователь вводит "ECMAScript",
+//то показать через alert: "Верно!"
+//в противном случае отобразить:"Не знаете? ECMAScript!"
 
-console.log();
+let input = prompt("Какое официальное название JavaScript?");
+if (input === "ECMAScript") {
+    alert("Верно!");
+} else {
+    alert("Не знаете? ECMAScript!");
+}
