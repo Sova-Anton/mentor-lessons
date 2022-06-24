7483c0b7f86c0bff0d005dec93a8c58c8a4b1732
Sova-Anton <anton.sova.goit@gmail.com>
2022-06-24 19:59:04 +0300

    +

--- js/script.js
+++ js/script.js
@@ -161,19 +161,19 @@ console.log(`${modifyHours}:${modifyMinutes}`);*/
 
 
 
-function fizzBuzz(num) {
-    console.log(num);
-    for (let i = 0; i <= num; i += 1) {
-        if (i % 3 === 0 && i % 5 === 0) {
-            console.log('fizzBuzz');
-        } else if (i % 3 === 0){
-           console.log('fizz');
-        } else if (i % 5 === 0) {
-            console.log('Buzz');
-        } else {
-            console.log(i)
-        }
-    }
-}
+// function fizzBuzz(num) {
+//     console.log(num);
+//     for (let i = 0; i <= num; i += 1) {
+//         if (i % 3 === 0 && i % 5 === 0) {
+//             console.log('fizzBuzz');
+//         } else if (i % 3 === 0){
+//            console.log('fizz');
+//         } else if (i % 5 === 0) {
+//             console.log('Buzz');
+//         } else {
+//             console.log(i)
+//         }
+//     }
+// }
 
-fizzBuzz(64);
+// fizzBuzz(64);
